b245f3d060c84fe8fc53f39430693a86f49d3259
Derek Chen <derekchen8783@gmail.com>
2018-02-01 09:37:14 +1300

    Fix header height bug

--- src/router/Header.js
+++ src/router/Header.js
@@ -1,7 +1,5 @@
 import React, { Component } from 'react';
-import {
-    Link
-  } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import '../css/Header.css';
 import logo from '../constant/images.jpeg';
 
@@ -11,43 +9,45 @@ class Header extends Component {
         this.state = {
             isToggleOn: true,
             mobileMode: this._checkMode(),
-            isHeaderFixed:false
-        };   
+            isHeaderFixed: false
+        };
     }
 
     _checkMode() {
         const width = window.innerWidth || document.body.clientWidth;
-        const isMobile = width < 600 ? true : false;//normally we need some constant width instead of 600 here
+        const isMobile = width < 800 ? true : false;//normally we need some constant width instead of 600 here
         return isMobile;
     }
 
     componentDidMount() {
         window.addEventListener("resize", this.updateDimensions.bind(this));
-        window.addEventListener("scroll",this.handleScoll.bind(this));
-        this.nonFixedHeaderHeight = document.getElementById('container').clientHeight;
+        window.addEventListener("scroll", this.handleScoll.bind(this));
+        
     }
 
     componentWillUnmount() {
         window.removeEventListener("resize", this.updateDimensions.bind(this));
-        window.removeEventListener("scroll",this.handleScoll.bind(this));
+        window.removeEventListener("scroll", this.handleScoll.bind(this));
     }
-    
+
     updateDimensions() {
+        this.nonFixedHeaderHeight = document.getElementById('container').clientHeight|| 0;
         this.setState(prevState => ({
             mobileMode: this._checkMode(),
         }));
     }
 
     handleScoll() {
-        console.log("height",this.nonFixedHeaderHeight);
+        this.nonFixedHeaderHeight = document.getElementById('container').clientHeight || 0;
+        console.log("header hight",this.nonFixedHeaderHeight);
         const headerFixed = window.scrollY < this.nonFixedHeaderHeight ? false : true;
         this.setState(prevState => ({
             isHeaderFixed: headerFixed
         }));
     }
 
-    shouldComponentUpdate(nextProp,nextState) {
-        if(JSON.stringify(nextState) === JSON.stringify(this.state)){
+    shouldComponentUpdate(nextProp, nextState) {
+        if (JSON.stringify(nextState) === JSON.stringify(this.state)) {
             return false;
         }
         return true;
@@ -55,31 +55,30 @@ class Header extends Component {
 
     handleClick() {
         this.setState(prevState => ({
-          isToggleOn: !prevState.isToggleOn,
+            isToggleOn: !prevState.isToggleOn,
         }));
-        console.log(JSON.stringify(this.state,null,2));
+        console.log(JSON.stringify(this.state, null, 2));
     }
 
-    render() {
+    renderNormalHeader() {
         return (
             <div className="app-header">
                 <div className="header-top-container" id='container'>
-                    <img className="app-logo" src={logo} alt="logo"/>
+                    <img className="app-logo" src={logo} alt="logo" />
                     <div className="header-search-bar">
                         <input type="text"></input>
-                        <button  
-                            onClick={()=>this.handleClick()}
-                            type="button" 
+                        <button
+                            onClick={() => this.handleClick()}
+                            type="button"
                             name="Search"
                         >
-                        {this.state.isToggleOn ? 'ON' : 'OFF'}
+                            {this.state.isToggleOn ? 'ON' : 'OFF'}
                         </button>
                     </div>
                 </div>
-                <div className={this.state.isHeaderFixed?"fixed-header":null}>
+                <div className={this.state.isHeaderFixed ? "fixed-header" : null}>
                     <nav >
                         <ul className="nav-bar">
-                            {this.state.mobileMode ? <li> this is mobile hearder</li> : null}
                             <li><Link to="/" >Home</Link></li>
                             <li><Link to="/a" >Group A</Link></li>
                             <li><Link to="/b" >Group B</Link></li>
@@ -90,6 +89,20 @@ class Header extends Component {
                     </nav>
                 </div>
             </div>
+        );
+    }
+
+    renderMobileHeader() {
+        return (
+            <div className="app-header" id='container'>
+                <p>mobile header comming soon...</p>
+            </div>
+        )
+    }
+
+    render() {
+        return (
+            this.state.mobileMode ? this.renderMobileHeader():this.renderNormalHeader()
         );
     }
 }
